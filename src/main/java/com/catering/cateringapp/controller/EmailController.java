ca2c30df4f51fbb4d848f1b88789a6dd829fd846
Mori656 <marcelnosek@o2.pl>
2024-06-09 15:01:14 +0200

    Mail test
    testowa wersja wysyłania maili bez gui

--- src/main/java/com/catering/cateringapp/controller/EmailController.java
+++ src/main/java/com/catering/cateringapp/controller/EmailController.java
@@ -1,4 +1,28 @@
 package com.catering.cateringapp.controller;
 
+import com.catering.cateringapp.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    public static void main(String[] args){
+        SpringApplication.run(EmailController.class,args);
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam String from, @RequestParam String subject, @RequestParam String text) {
+        from = "marcelnose@o2.pl";
+        subject = "test starego";
+        text = "twój stary działą";
+        emailService.sendSimpleMessage(from, subject, text);
+        return "Email sent successfully";
+    }
 }
